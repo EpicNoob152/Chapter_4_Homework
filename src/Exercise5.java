4362e14313968d9524075f87a91cd7f012b75f00
Jaylan Igbinoba <jigbinoba@ggc.edu>
2023-03-03 16:11:39 -0500

    DEALING WITH STRINGS AHHHHH

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,19 +1,32 @@
+/**
+ * Class: Exercise 1
+ * @author: Jaylan Igbinoba
+ * @Course: ITEC 2140 Spring 2023
+ * @verison: 1.0
+ * @Written: February 28, 2023
+ * @Description: This code tries to output the factorial of the inputted number.
+ */
 import java.util.Scanner;
 public class Exercise5 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int number = input.nextInt();
-        int product = 0;
-        int i = 0;
-        while(i <= number ){
-            i++;
-            product *= i;
 
+        //Initial values of the product and i values before they get changed.
+        int number = input.nextInt();
+        int product = 1;
+        int i = 1;
+
+        //Below is the code that times all the numbers that lead up to the user inputted number.
+        while(i <= number ){
+
+            product *= i;
+            i++;
 
         }
-        System.out.println(product + "!");
+        //This prints the factorial.
+        System.out.println(number + "!" + " is: " + product);
     }
 
 }
---
+++ src/Exercise7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the 1st string ");
+        String word1 = input.nextLine();
+
+
+    }
+}
